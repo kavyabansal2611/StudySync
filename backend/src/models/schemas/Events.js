a9c8784e1375f3e2d9205b227cd5508d11f6aafe
kavyabansal2611 <bansalkavya90@gmail.com>
2026-06-15 14:26:53 +0530

    Schemas and User methods added inside model

---
+++ backend/src/models/schemas/Events.js
@@ -0,0 +1,12 @@
+import mongoose,{ Schema,model } from 'mongoose';
+
+const EventSchema = new Schema({
+    user: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+    title: { type: String, required: true },
+    description: { type: String, required: false },
+    event_start: { type: Date, required: true },
+    event_end: { type: Date, required: false },
+    location: { type: String, required: false },
+}, { timestamps: true });
+
+export const Event = model('Event', EventSchema);
---
+++ backend/src/models/schemas/Tasks.js
@@ -0,0 +1,12 @@
+import mongoose,{ Schema,model } from 'mongoose';
+
+const TaskSchema = new Schema({
+    user: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+    title: { type: String, required: true },
+    description: { type: String, required: true },
+    deadline: { type: Date, required: true },
+    status: { type: String, enum: ['pending', 'completed', 'cancelled'], default: 'pending' },
+    priority: { type: Number, required: true, min: 1, max: 10 },
+}, { timestamps: true });
+
+export const Task = model('Task', TaskSchema);
