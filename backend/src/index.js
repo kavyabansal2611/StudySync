1670467a02cc6b3aa696340d96c3d75a492c3d6b
Kavya bansal <bansalkavya90@gmail.com>
2026-06-28 02:57:10 +0530

    Use environment variable for server port
    Updated server to use environment variable for port.

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,12 +5,9 @@ import { connectDB } from './db/connect.js';
 connectDB()
     .then(() => {
         console.log('Connected to the database');
-        const PORT = 3000;
-        const HOST = '127.0.0.1'; // Restricts access strictly to your local machine
-
-        app.listen(PORT, HOST, () => {
-         console.log(`Server is running locally on http://${HOST}:${PORT}`);
-        });
+           const PORT = process.env.SERVER_PORT || 3000;
+            app.listen(PORT, () => {
+            console.log(`Server is running on port ${PORT}`); // Restricts access strictly to your local machine
     })
     .catch((error) => {
         console.error('Failed to connect to the database:', error);
